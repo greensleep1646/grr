f3e6c08d9d909f50347b10cf9631c1ff11a214d3
jsjdndjndmsn <152083411+greensleep1646@users.noreply.github.com>
2024-04-07 14:55:35 -0400

    Create grr.py

---
+++ grr.py
@@ -0,0 +1,24 @@
+#Xractz
+#IndoSec
+
+import json, re
+from requests import Session
+s = Session()
+
+print("RESET PASSWORD IG by Xractz")
+id = input("\nEnter your username/email/phone number: ")
+
+url = "https://www.instagram.com/accounts/account_recovery_send_ajax/"
+
+g = s.get("https://www.instagram.com/accounts/password/reset/").text
+token = re.search(r'csrf_token":"',g).group(1)
+
+headers = {
+    'x-csrftoken': token
+}
+
+data = {
+	'email_or_username':id
+	,'recaptcha_challenge_field':''
+}
+
